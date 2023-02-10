e1a46f5ebb53c572ec9db773d6b57c2ba0caf4ca
cmhhelgeson <62450112+cmhhelgeson@users.noreply.github.com>
2023-02-09 17:59:56 -0800

    Removed dist from git

--- dist/database/environment/envVars.js
+++ dist/database/environment/envVars.js
@@ -4,6 +4,6 @@ export const psqlDatabase = "LC_Slice";
 export const DATABASE_URL = "postgresql://postgres:v0ikEtVTWsl6QLXfckVZ@containers-us-west-145.railway.app:7130/railway";
 export const PGDATABASE = "railway";
 export const PGHOST = "containers-us-west-145.railway.app";
-export const PGPASSWORD = "v0ikEtVTWsl6QLXfckVZ";
+export const PGPASSWORD = "HbebnmwVaW9JCBgwO4uz";
 export const PGPORT = "7130";
 export const PGUSER = "postgres";
