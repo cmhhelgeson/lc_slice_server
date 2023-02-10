9490f6832318dcc900d934f93202124acde0525a
cmhhelgeson <62450112+cmhhelgeson@users.noreply.github.com>
2023-02-09 19:01:11 -0800

    Updated dist

--- dist/database/environment/envVars.js
+++ dist/database/environment/envVars.js
@@ -6,5 +6,4 @@ export const PGHOST = "containers-us-west-145.railway.app";
 export const PGPASSWORD = "p03UMzZNVYOlp3Hgc0KQ";
 export const PGPORT = "7130";
 export const PGUSER = "postgres";
-export const DATABASE_URL = `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}"
-;
+export const DATABASE_URL = `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}`;
